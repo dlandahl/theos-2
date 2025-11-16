8cf607ce7c066f99315aad40a0717df106a253f8
dlandahl <landahl2014@gmail.com>
2025-11-16 11:50:12 +0000

    uACPI less optimized build

--- kernel/modules/Uacpi/build.sh
+++ kernel/modules/Uacpi/build.sh
@@ -1,3 +1,3 @@
-gcc source_code/source/*.c -Isource_code/include -c -mno-red-zone -mcmodel=kernel -fno-pie -O3
+gcc source_code/source/*.c -Isource_code/include -c -mno-red-zone -mcmodel=kernel -fno-pie -O1
 ar rcs uacpi.a *.o
 rm *.o
