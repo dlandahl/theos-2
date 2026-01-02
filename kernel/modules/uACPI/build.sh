3ab65ff6c382efa4e2b9ca6afc626e5390f77765
dlandahl <36662196+dlandahl@users.noreply.github.com>
2026-01-02 14:53:31 +0000

    Remove our ACPI code in favour of uACPI

--- kernel/modules/uACPI/build.sh
+++ kernel/modules/uACPI/build.sh
@@ -1,3 +1,3 @@
-gcc source_code/source/*.c -Isource_code/include -c -mno-red-zone -mcmodel=kernel -fno-pie -O1
+gcc source_code/source/*.c -Isource_code/include -c -mno-red-zone -mcmodel=kernel -fno-pie -mno-tls-direct-seg-refs -O1 -fno-stack-protector
 ar rcs uacpi.a *.o
 rm *.o
